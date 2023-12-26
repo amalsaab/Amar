a7b1b17dbb74ea7c9eb820e55add66f69cd90ad5
amalsaab <441102683@student.ksu.edu.sa>
2023-12-26 14:01:36 +0300

    init BottomNav

--- Aqarati-app/src/App.jsx
+++ Aqarati-app/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Landing from './Compnent/Landing'
 import { Routes, Route } from 'react-router-dom'
 import Qu from './Compnent/Qu'
+import BottomNavigation from './Compnent/navigation/BottomNavigation'
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
     
 
       <Routes>
+        { /* For component testing */}
+        <Route path="/" element={<BottomNavigation/>}/>
+
         <Route path="/" element={<Landing/>}/>
         <Route path="/Q" element={<Qu/>}/>
       </Routes>
